03c22f9d58573b0b17974fbfe32cc7bba4e71f0d
nh-sue <samuel.ueltschi@heimstein.rocks>
2025-06-06 13:20:47 +0200

    cleanup dead company webpages

--- pages/ortschaft/[slug].tsx
+++ pages/ortschaft/[slug].tsx
@@ -137,34 +137,44 @@ export default function Gemeinde(props: GemeindeProps) {
                 <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                   {firmen.map((f) => (
                     <div className="shadow-md rounded flex flex-col" key={f.id}>
-                      <a key={f.id} href={f.webpage} className="flex gap-3 p-3 text-sm" target="_blank">
-                        <div className="flex-1">
+                      {f.webpage ? (
+                        <a key={f.id} href={f.webpage} className="flex gap-3 p-3 text-sm" target="_blank">
+                          <div className="flex-1">
+                            {f.name}
+                            <br />
+                            {f.strasse}
+                            <br />
+                            {f.plz} {f.ort}
+                          </div>
+
+                          <div className="flex flex-col">
+                            <svg
+                              className="w-6 h-6 text-gray-800"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="24"
+                              height="24"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeWidth="2"
+                                d="M4.37 7.657c2.063.528 2.396 2.806 3.202 3.87 1.07 1.413 2.075 1.228 3.192 2.644 1.805 2.289 1.312 5.705 1.312 6.705M20 15h-1a4 4 0 0 0-4 4v1M8.587 3.992c0 .822.112 1.886 1.515 2.58 1.402.693 2.918.351 2.918 2.334 0 .276 0 2.008 1.972 2.008 2.026.031 2.026-1.678 2.026-2.008 0-.65.527-.9 1.177-.9H20M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                              />
+                            </svg>
+                          </div>
+                        </a>
+                      ) : (
+                        <div className="p-3 text-sm">
                           {f.name}
                           <br />
                           {f.strasse}
                           <br />
                           {f.plz} {f.ort}
                         </div>
-
-                        <div className="flex flex-col">
-                          <svg
-                            className="w-6 h-6 text-gray-800"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              stroke="currentColor"
-                              strokeLinecap="round"
-                              strokeWidth="2"
-                              d="M4.37 7.657c2.063.528 2.396 2.806 3.202 3.87 1.07 1.413 2.075 1.228 3.192 2.644 1.805 2.289 1.312 5.705 1.312 6.705M20 15h-1a4 4 0 0 0-4 4v1M8.587 3.992c0 .822.112 1.886 1.515 2.58 1.402.693 2.918.351 2.918 2.334 0 .276 0 2.008 1.972 2.008 2.026.031 2.026-1.678 2.026-2.008 0-.65.527-.9 1.177-.9H20M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                            />
-                          </svg>
-                        </div>
-                      </a>
+                      )}
                       <div className="flex-1"></div>
                       {f.plannerlink ? (
                         <div className="flex align-middle justify-center p-2">
